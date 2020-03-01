36f219d6f0365f2f8ba65171402a63049ecde72a
Олег Дутченко <dutchenko.o.dev@gmail.com>
2020-03-02 00:26:44 +0200

    Add docs

---
+++ docs-src/index.md
@@ -0,0 +1,70 @@
+# Hello
+
+## Functions
+
+### `microbe-strip-units()`
+
+`insertMixinFn=core/functions#microbe-strip-units`
+
+---
+
+### `microbe-em()`
+
+---
+
+`insertMixinFn=core/functions#microbe-em`
+
+### `microbe-rem()`
+
+---
+
+`insertMixinFn=core/functions#microbe-rem`
+
+---
+
+## Mixins
+
+### `microbe-absolute-center`
+
+`insertMixinFn=core/mixins#microbe-absolute-center`
+
+---
+
+### `microbe-absolute-gap`
+
+`insertMixinFn=core/mixins#microbe-absolute-gap`
+
+---
+
+### `microbe-absolute-square`
+
+`insertMixinFn=core/mixins#microbe-absolute-square`
+
+---
+
+### `microbe-flex-cell-width`
+
+`insertMixinFn=core/mixins#microbe-flex-cell-width`
+
+---
+
+### `microbe-media`
+
+`insertMixinFn=core/mixins#microbe-media`
+
+---
+
+## Variables
+
+### Spaces
+
+`insertVariablesTable`
+`insertVariableRow=core/defaults#microbe-space-xxs`
+`insertVariableRow=core/defaults#microbe-space-xs`
+`insertVariableRow=core/defaults#microbe-space-sm`
+`insertVariableRow=core/defaults#microbe-space-md`
+`insertVariableRow=core/defaults#microbe-space-df`
+`insertVariableRow=core/defaults#microbe-space-lg`
+`insertVariableRow=core/defaults#microbe-space-xlg`
+`insertVariableRow=core/defaults#microbe-space-xxl`
+`insertVariableRow=core/defaults#microbe-space-hg`
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 // Deps
 // -----------------------------------------------------------------------------
 
+const fs = require('fs');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const notify = require('gulp-notify');
 const postcss = require('gulp-postcss');
 const sortMQ = require('postcss-sort-media-queries');
 const mobileFirst = require('sort-css-media-queries');
-const browserSync = require('browser-sync');
 
 // -----------------------------------------------------------------------------
 // Sass
@@ -16,18 +16,23 @@ const browserSync = require('browser-sync');
 
 sass.compiler = require('node-sass');
 const renderSass = () => {
-	return gulp.src('./src/bundle.scss')
-		.pipe(sass({
-			outputStyle: 'expanded',
-			indentType: 'tab',
-			indentWidth: 1
-		}).on('error', notify.onError('Error: <%= error.message %>')))
-		.pipe(postcss([
-			sortMQ({
-				sort: mobileFirst
-			})
-		]))
-		.pipe(gulp.dest('./dist'))
+	return gulp
+		.src('./src/bundle.scss')
+		.pipe(
+			sass({
+				outputStyle: 'expanded',
+				indentType: 'tab',
+				indentWidth: 1
+			}).on('error', notify.onError('Error: <%= error.message %>'))
+		)
+		.pipe(
+			postcss([
+				sortMQ({
+					sort: mobileFirst
+				})
+			])
+		)
+		.pipe(gulp.dest('./dist'));
 };
 
 // -----------------------------------------------------------------------------
@@ -35,13 +40,125 @@ const renderSass = () => {
 // -----------------------------------------------------------------------------
 
 const renderSassIncremental = () => {
-	const bs = browserSync.create();
-	bs.init({
-		server: {
-			baseDir: 'dist'
-		}
-	});
+	const bs = require('browser-sync').create();
+	bs.init({ server: true });
 	return gulp.watch('./src/**', renderSass).on('change', bs.reload);
+};
+
+// -----------------------------------------------------------------------------
+// Docs
+// -----------------------------------------------------------------------------
+
+const generateSassDocs = () => {
+	const sassDoc = require('sassdoc');
+	const cacheFolder = './.cache';
+	const cacheJSON = './.cache/sass-doc.json';
+
+	const transform = (data) => {
+		const newData = {};
+		data.forEach((doc) => {
+			const { group = [] } = doc;
+			const name = group[0];
+			if (name !== undefined && name.length) {
+				newData[name] = doc;
+			}
+		});
+		return newData;
+	};
+
+	return sassDoc.parse('./src', { verbose: true }).then((data) => {
+		if (!fs.existsSync(cacheFolder)) {
+			fs.mkdirSync(cacheFolder);
+		}
+		const json = transform(data);
+		fs.writeFileSync(cacheJSON, JSON.stringify(json, undefined, '\t'));
+	});
+};
+
+const generateMarkdownDocs = () => {
+	const sassDocData = require('./.cache/sass-doc');
+
+	const mdCode = (code) => `\`${code}\``;
+	const mdPre = (code, lang) => `\`\`\`${lang}\n${code}\n\`\`\``;
+	const mdTD = (row) => `| ${row.map((r) => r.replace(/\|/g, '/')).join(' | ')} |`;
+	const mdTH = (row) => [mdTD(row), mdTD(row.map(() => '---'))].join('\n');
+	const generateTable = (head, ...rows) => [mdTH(head), ...rows.map(mdTD)].join('\n');
+
+	const insertMixinFn = /`insertMixinFn=(.+)`/g;
+	const replacerMixinFn = (sample, group) => {
+		const doc = sassDocData[group];
+		if (doc === undefined) {
+			return '';
+		}
+		const info = [doc.description];
+
+		const parameters = doc.parameter;
+		if (Array.isArray(parameters) && parameters.length) {
+			info.push(
+				'##### Parameters',
+				generateTable(
+					['Name', 'Description', 'Type', 'Default value'],
+					...parameters.map(({ type, name, description, default: def }) => {
+						return [
+							name,
+							description || '---',
+							mdCode(type),
+							def !== undefined ? mdCode(def) : '---'
+						];
+					})
+				)
+			);
+		}
+
+		if (doc.return !== undefined) {
+			const _return = doc.return;
+			info.push(
+				'##### Returns',
+				`${mdCode(_return.type)}${
+					_return.description ? ' - ' + _return.description : ''
+				}`
+			);
+		}
+
+		const examples = doc.example;
+		if (Array.isArray(examples) && examples.length) {
+			info.push(
+				'##### Examples',
+				...examples.map(({ type, code }) => mdPre(code, type || 'scss'))
+			);
+		}
+
+		return info.join('\n\n');
+	};
+
+	const insertVariablesTable = /`insertVariablesTable`/g;
+	const replacerVariablesTable = (sample) => {
+		return mdTH(['Name', 'Description', 'Value', 'Scope']);
+	};
+
+	const insertVariableRow = /`insertVariableRow=(.+)`/g;
+	const replacerVariableRow = (sample, group) => {
+		const doc = sassDocData[group];
+		if (doc === undefined) {
+			return '';
+		}
+		const description = doc.description.replace(/\r?\n$/, '');
+		const { name, value, scope } = doc.context;
+		return mdTD([mdCode('$' + name), description, mdCode(value), scope]);
+	};
+
+	return gulp
+		.src('./docs-src/**/*.md')
+		.on('data', (file) => {
+			const content = file._contents.toString();
+			file._contents = Buffer.from(
+				content
+					.replace(insertVariablesTable, replacerVariablesTable)
+					.replace(insertVariableRow, replacerVariableRow)
+					.replace(insertMixinFn, replacerMixinFn)
+			);
+		})
+		.pipe(gulp.dest('./docs'));
 };
 
 // -----------------------------------------------------------------------------
@@ -50,3 +167,5 @@ const renderSassIncremental = () => {
 
 exports.sass = renderSass;
 exports.watch = renderSassIncremental;
+exports['sass-doc'] = generateSassDocs;
+exports['md-doc'] = generateMarkdownDocs;
